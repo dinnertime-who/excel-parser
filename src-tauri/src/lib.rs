a0d3391f88b39804451ec5d21dce97c8d2c6ab4e
dinnertime <dinnertime.dev@gmail.com>
2025-01-15 20:09:21 +0900

    송장번호 매칭기 기능

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,106 +1,12 @@
-use calamine::{open_workbook_auto, Reader};
-use xlsxwriter::*;
 use std::fs;
 use std::env;
 use tauri::command;
 
 #[command]
-fn process_excel_from_upload(file_bytes: Vec<u8>, file_name: String) -> Result<String, String> {
-    // 업로드된 파일을 임시 파일로 저장
-    let temp_file_path = env::temp_dir().join(file_name);
-    if let Err(e) = fs::write(&temp_file_path, file_bytes) {
-        return Err(format!("파일 저장 실패: {}", e));
-    }
-
+fn download_file(file_bytes: Vec<u8>, file_name: String) -> Result<(), String> {
     // 다운로드 폴더 경로 설정
-    let output_path = env::var("HOME").unwrap_or_else(|_| "./".to_string()) + "/Downloads/gimhae_members_valid.xlsx";
-
-    // 첫 번째 시트를 가져오기
-    let sheet = get_first_sheet(temp_file_path.to_str().unwrap()).ok_or_else(|| "파일을 읽을 수 없습니다.".to_string())?;
-
-    // 필터링된 행 가져오기
-    let rows = filter_rows(sheet);
-
-    // 결과 엑셀 파일 생성
-    match make_excel_from_json(rows, &output_path) {
-        Ok(_) => Ok(output_path),
-        Err(e) => Err(format!("엑셀 파일 생성 중 오류 발생: {}", e)),
-    }
-}
-
-fn get_first_sheet(file_path: &str) -> Option<Vec<Vec<String>>> {
-    // 파일 존재 여부 확인
-    if !fs::metadata(file_path).is_ok() {
-        eprintln!("파일이 존재하지 않습니다: {}", file_path);
-        return None;
-    }
-
-    // 엑셀 파일 읽기
-    let mut workbook = match open_workbook_auto(file_path) {
-        Ok(wb) => wb,
-        Err(e) => {
-            eprintln!("엑셀 파일을 읽는 도중 오류가 발생했습니다: {}", e);
-            return None;
-        }
-    };
-
-    if let Some(Ok(range)) = workbook.worksheet_range_at(0) {
-        // 데이터를 2D 벡터로 변환
-        let data = range
-            .rows()
-            .map(|row| row.iter().map(|cell| cell.to_string()).collect::<Vec<String>>())
-            .collect();
-        return Some(data);
-    }
-
-    eprintln!("엑셀 시트가 없습니다.");
-    None
-}
-
-fn filter_rows(rows: Vec<Vec<String>>) -> Vec<Vec<String>> {
-    if rows.is_empty() {
-        return vec![];
-    }
-
-    // 첫 번째 행(헤더)에서 "휴대폰번호" 컬럼의 인덱스 찾기
-    let header = &rows[0];
-    let phone_column_index = header.iter().position(|col| col == "휴대폰번호");
-
-    match phone_column_index {
-        Some(index) => rows
-            .into_iter()
-            .skip(1) // 헤더 제외
-            .filter(|row| {
-                if let Some(phone) = row.get(index) {
-                    phone.starts_with("01")
-                } else {
-                    false
-                }
-            })
-            .collect(),
-        None => {
-            eprintln!("'휴대폰번호' 컬럼을 찾을 수 없습니다.");
-            vec![]
-        }
-    }
-}
-
-fn make_excel_from_json(data: Vec<Vec<String>>, output_path: &str) -> Result<(), XlsxError> {
-    let workbook_result = Workbook::new(output_path);
-    let workbook = match workbook_result {
-        Ok(wb) => wb,
-        Err(e) => return Err(e),
-    };
-
-    let mut sheet = workbook.add_worksheet(None)?;
-
-    for (row_idx, row) in data.iter().enumerate() {
-        for (col_idx, cell) in row.iter().enumerate() {
-            sheet.write_string(row_idx as u32, col_idx as u16, cell, None)?;
-        }
-    }
-
-    workbook.close()?;
+    let output_path = env::var("HOME").unwrap_or_else(|_| "./".to_string()) + "/Downloads/" + &file_name;
+    fs::write(output_path, file_bytes).map_err(|e| e.to_string())?;
     Ok(())
 }
 
@@ -108,7 +14,7 @@ fn make_excel_from_json(data: Vec<Vec<String>>, output_path: &str) -> Result<(),
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![process_excel_from_upload])
+        .invoke_handler(tauri::generate_handler![download_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,129 @@
-import { useRef, useState } from 'react';
-import { invoke } from '@tauri-apps/api/core';
+import { useRef } from 'react';
 import './App.css';
+import ExcelJS from 'exceljs';
+import { invoke } from '@tauri-apps/api/core';
+
+function worksheetToJson(worksheet: ExcelJS.Worksheet) {
+  const data: Record<string, any>[] = [];
+  worksheet.eachRow((row, rowNumber) => {
+    if (rowNumber === 1) return;
+
+    const rowData: Record<string, any> = {};
+    row.eachCell((cell, colNumber) => {
+      const header = worksheet.getRow(1).getCell(colNumber).value;
+      if (typeof header === 'string') {
+        rowData[header] = cell.value;
+      }
+    });
+    data.push(rowData);
+  });
+
+  return data;
+}
+
+const DILIVERY_CODE_MAP: Record<string, any> = {
+  CJ대한통운: 2,
+  우체국택배: 3,
+  한진택배: 4,
+  롯데택배: 5,
+  로젠택배: 6,
+  KG로지스: 7,
+  CVSnet: 8,
+  KGB택배: 9,
+  경동택배: 10,
+  대신택배: 11,
+  일양로지스: 12,
+  GTX로지스: 13,
+  천일택배: 15,
+  건영택배: 14,
+  ['직접배송/수령']: 29,
+  합동택배: 28,
+  농협택배: 27,
+};
+
+async function getWorksheetFromFile(file: File) {
+  const arrayBuffer = await file.arrayBuffer();
+  const workbook = new ExcelJS.Workbook();
+  const readResult = await workbook.xlsx.load(arrayBuffer);
+
+  const worksheet = readResult.getWorksheet(1);
+  return worksheet;
+}
 
 function App() {
-  const [savedPaths, setSavedPaths] = useState<string[]>([]);
-  const excelInput = useRef<HTMLInputElement>(null);
+  const bosExcelInput = useRef<HTMLInputElement>(null);
+  const erpExcelInput = useRef<HTMLInputElement>(null);
 
   async function processExcel() {
-    if (!excelInput.current || !excelInput.current.files) return;
+    if (!bosExcelInput.current || !bosExcelInput.current.files) return;
+    if (!erpExcelInput.current || !erpExcelInput.current.files) return;
 
-    for (const file of excelInput.current.files) {
-      const reader = new FileReader();
-      reader.onload = async function () {
-        const arrayBuffer = reader.result;
-        if (!arrayBuffer || typeof arrayBuffer === 'string') return;
+    const bosExcelFile = bosExcelInput.current.files[0];
+    if (!bosExcelFile) return;
 
-        const fileBytes = Array.from(new Uint8Array(arrayBuffer));
-        const result = await invoke('process_excel_from_upload', {
-          fileBytes: fileBytes,
-          fileName: file.name,
-        });
-        setSavedPaths((prev) => [...prev, result as string]);
-      };
-      reader.readAsArrayBuffer(file);
+    const erpExcelFile = erpExcelInput.current.files[0];
+    if (!erpExcelFile) return;
+
+    const bosWorkSheet = await getWorksheetFromFile(bosExcelFile);
+    const erpWorkSheet = await getWorksheetFromFile(erpExcelFile);
+
+    if (!bosWorkSheet || !erpWorkSheet) return;
+
+    const bosExcelData = worksheetToJson(bosWorkSheet);
+    const erpExcelData = worksheetToJson(erpWorkSheet);
+
+    const newWorkBook = new ExcelJS.Workbook();
+    const worksheet = newWorkBook.addWorksheet('Sheet 1');
+    worksheet.columns = Object.keys(bosExcelData[0]).map((key) => ({
+      header: key,
+      key,
+    }));
+
+    for (const row of bosExcelData) {
+      const lookuped = erpExcelData.find((data, i) => {
+        return `${data['주문상세번호']}` === `${row['품목별주문번호']}`;
+      });
+
+      if (!lookuped) {
+        continue;
+      }
+      row['운송장번호'] = `${lookuped['송장번호']}`;
+      row['배송사코드'] = DILIVERY_CODE_MAP[`${lookuped['택배사']}`] || 0;
+      worksheet.addRow(row);
     }
+
+    // 워크북을 ArrayBuffer로 변환
+    const buffer = await newWorkBook.xlsx.writeBuffer();
+    const uint8Array = new Uint8Array(buffer);
+
+    await invoke('download_file', {
+      fileBytes: uint8Array,
+      fileName: `김해온몰_송장번호_매칭엑셀_${new Date().toLocaleString()}.xlsx`,
+    });
   }
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>송장 번호 매칭기</h1>
 
       <form
-        className="row"
+        className=""
         onSubmit={(e) => {
           e.preventDefault();
           processExcel();
         }}
+        style={{ display: 'flex', flexDirection: 'column' }}
       >
-        <input type="file" ref={excelInput} />
+        <div className="">
+          <label htmlFor="">BOS 주문엑셀</label>
+          <input type="file" ref={bosExcelInput} />
+        </div>
+        <div className="">
+          <label htmlFor="">ERP 주문엑셀</label>
+          <input type="file" ref={erpExcelInput} />
+        </div>
         <button type="submit">엑셀 처리하기</button>
       </form>
-
-      <div>
-        {savedPaths.map((path) => (
-          <div>{`저장완료: ${path}`}</div>
-        ))}
-      </div>
     </main>
   );
 }
